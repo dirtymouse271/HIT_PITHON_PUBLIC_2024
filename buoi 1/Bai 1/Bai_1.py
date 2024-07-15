33fa042719fc67e0ece678874715b75480cd5b14
dirtymouse271 <cheetahspeedyv@gmail.com>
2024-07-15 19:26:04 +0700

    123

--- buoi 1/Bai 1/Bai_1.py
+++ buoi 1/Bai 1/Bai_1.py
@@ -1,2 +1,28 @@
-a=1000
-print(a)
+﻿'''import pprint
+a = 2023
+b= 2+4j
+print(type(b))
+print("a=",2023)
+x =float(input("moi ban nhap so x="))
+print("Số a bạn vừa nhập là :",x)
+c="mystring"
+b= 5
+print(c[0:2])
+a= "I love Python"
+my_list = [1, 2, 3, 'hello']
+print(my_list[2])
+print(my_list[1:4])
+my_dict = {
+    "keys":"values",
+    "keys1":"value1s",
+    "keys2":"value1s",
+    "keys3":"value3s",
+    "keys4":"value4s",
+    }
+print("my_dict=",my_dict)
+print("Type",type(my_dict))
+'''
+# Toán tử so sánh 
+a =5
+print(a==10)
+print(a!=10)
